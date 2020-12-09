3893a5e858e1773b6a59d13f467218a9eefaf053
991296755@qq.com <lyj112233>
2020-12-09 14:27:33 +0800

    class继承与访问控制符

---
+++ js/3.类.js
@@ -0,0 +1,62 @@
+class Person {
+    info() {
+        // 成员方法只能使用成员变量
+        console.log(`${this.name} - ${this.age}`);
+    }
+    static staticInfo() {
+        // 静态方法只能使用静态变量
+        console.log(`${this.country} - ${this.planet}`);
+    }
+}
+Person.country = 'China';
+Person.planet = 'Earth';
+const person = new Person();
+person.name = 'Jane';
+person.age = 18;
+person.info();
+Person.staticInfo();
+// ts中的继承
+class Student extends Person {
+    studentInfo() {
+        console.log(`${this.schoolName} - ${this.studentNo}`);
+    }
+    info() {
+        super.info();
+    }
+}
+const student = new Student();
+student.name = 'Sam';
+student.age = 19;
+student.studentNo = '200129312301';
+student.schoolName = '麻省理工学院';
+student.studentInfo();
+student.info();
+// 属性的访问控制符
+// public: 当前类, 子类, 外部类均能访问, 不写默认为public
+// protected: 当前类, 子类才能访问
+// private: 只有在当前类中才能访问
+class Man extends Student {
+    constructor() {
+        super(...arguments);
+        this.gender = '男';
+    }
+    changeGender() {
+        // private属性只有在当前类中才能访问
+        this.gender = '女';
+        console.log(this.gender);
+    }
+}
+const man = new Man();
+// 只能访问public的属性, 其他的属性访问编译会出错
+man.height = 180;
+man.changeGender();
+class Boy extends Man {
+    access() {
+        // 子类能访问父类的protected属性
+        this.penisLength = 20;
+        console.log(`${this.height} - ${this.penisLength}`);
+    }
+}
+const boy = new Boy();
+boy.height = 140;
+boy.access();
---
+++ ts/3.类.ts
@@ -0,0 +1,86 @@
+class Person {
+  name: string
+
+  age: number
+
+  static country: string = 'China'
+
+  static planet: string = 'Earth'
+
+  info(): void {
+    // 成员方法只能使用成员变量
+    console.log(`${this.name} - ${this.age}`)
+  }
+
+  static staticInfo(): void {
+    // 静态方法只能使用静态变量
+    console.log(`${this.country} - ${this.planet}`)
+  }
+}
+
+const person = new Person();
+person.name = 'Jane'
+person.age = 18
+person.info()
+Person.staticInfo()
+
+// ts中的继承
+class Student extends Person {
+
+  studentNo: string
+
+  schoolName: string
+
+  studentInfo(): void {
+    console.log(`${this.schoolName} - ${this.studentNo}`)
+  }
+
+  info(): void {
+    super.info();
+  }
+}
+
+const student = new Student()
+student.name = 'Sam'
+student.age = 19
+student.studentNo = '200129312301'
+student.schoolName = '麻省理工学院'
+student.studentInfo()
+student.info()
+
+// 属性的访问控制符
+// public: 当前类, 子类, 外部类均能访问, 不写默认为public
+// protected: 当前类, 子类才能访问
+// private: 只有在当前类中才能访问
+class Man extends Student {
+
+  private gender: string = '男'
+
+  public height: number
+
+  protected penisLength: number
+
+  changeGender(): void {
+    // private属性只有在当前类中才能访问
+    this.gender = '女'
+    console.log(this.gender)
+  }
+
+}
+
+const man = new Man();
+// 只能访问public的属性, 其他的属性访问编译会出错
+man.height = 180
+man.changeGender()
+
+class Boy extends Man {
+
+  access(): void {
+    // 子类能访问父类的protected属性
+    this.penisLength = 20
+    console.log(`${this.height} - ${this.penisLength}`)
+  }
+}
+const boy = new Boy()
+boy.height = 140
+boy.access()
